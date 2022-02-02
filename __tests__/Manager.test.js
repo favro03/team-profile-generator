e75eddacca059e140c2d840e673f4b92acd4afbb
tfavro <wetr9902@gmail.com>
2022-02-01 22:12:42 -0600

    added tests and methods for manager object

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,13 @@
 const Manager = require('../lib/Manager');
-
-jest.mock('../lib/Manager');
-
+//creates a manager object
 test('create an Manager object', () =>{
-    const manager = new Manager();
+    const manager = new Manager('Tracy', 100, 'wetr9902@gmail.com', 322);
 
-    expect(manager.officeNumber).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
+//gets from getRole
+test('gets role',() =>{
+    const manager = new Manager('Tracy', 100, 'wetr9902@gmail.com', 322);
+
+    expect(manager.getRole()).toEqual('Manager');
+})
