7f06a8b178764b2e87e4deb0573d27ce04b2d5cb
tfavro <wetr9902@gmail.com>
2022-02-01 22:04:32 -0600

    added tests and methods for intern object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,17 @@
 const Intern = require('../lib/Intern');
-
-jest.mock('../lib/Intern');
-
+//creates an intern object
 test('create an Intern object', () =>{
-    const intern = new Intern();
+    const intern = new Intern('Tracy', 100, 'wetr9902@gmail.com','MSU');
     expect(intern.school).toEqual(expect.any(String));
+})
+//gets school from getSchool()
+test('get school',() => {
+    const intern = new Intern('Tracy', 100, 'wetr9902@gmail.com','MSU');
+    expect(intern.getSchool()).toEqual(expect.any(String));
+})
 
+//gets role from getRole()
+test('gets role',() =>{
+    const intern = new Intern('Tracy', 100, 'wetr9902@gmail.com','MSU');
+    expect(intern.getRole()).toEqual('Intern');
 })
