4e7aebe678170431539ff9e0d774f28dfe0f1fd5
tfavro <wetr9902@gmail.com>
2022-02-01 21:33:10 -0600

    added tests and methods for employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,39 @@
 const Employee = require('../lib/Employee');
-jest.mock('../lib/Employee');
 
+//creates an employee object
 test('create an Employee object',() =>{
     
-    const employee = new Employee();
-    
+    const employee = new Employee('Tracy', 100, 'wetr9902@gmail.com');
     
     expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-
 });
+
+//gets name from getName()
+test('gets employee name',() =>{
+    const employee = new Employee('Tracy', 100, 'wetr9902@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+})
+
+//gets id from getId()
+test('gets id number',() =>{
+    const employee = new Employee('Tracy', 100, 'wetr9902@gmail.com')
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+//gests email from getEmail()
+test('gets email address',() =>{
+    const employee = new Employee('Tracy', 100, 'wetr9902@gmail.com')
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+})
+//gets role from input from getRole()
+test('gets role from user input',() =>{
+    const employee = new Employee('Tracy', 100, 'wetr9902@gmail.com');
+    expect(employee.getRole()).toEqual('Employee');
+    
+})
+
+
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,9 @@ const Engineer = require('../lib/Engineer');
 
 jest.mock('../lib/Engineer');
 
+//test to create a
 test('create an Engineer object',() =>{
     const engineer = new Engineer();
-    expect(engineer.userName).toEqual(expect.any(String));
+    expect(engineer.gitHub).toEqual(expect.any(String));
 });
+
